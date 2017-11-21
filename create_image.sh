f2ff052151f3a54115a1efe460e2992469073a5b
K RND <yonbanki@gmail.com>
2017-11-21 19:54:12 +0900

    create_image.shにブラックリスト除去処理を追加

--- create_image.sh
+++ create_image.sh
@@ -1,11 +1,11 @@
-rm -f faces
-rm -f faces64
-rm -f faces64_edge
-rm -f faces65_gray
-rm -f faces-np
-rm -f faces-np64
-rm -f faces-np64_edge
-rm -f faces-np64_gray
+rm -rf faces
+rm -rf faces64
+rm -rf faces64_edge
+rm -rf faces65_gray
+rm -rf faces-np
+rm -rf faces-np64
+rm -rf faces-np64_edge
+rm -rf faces-np64_gray
 mkdir faces
 mkdir faces64
 mkdir faces64_edge
@@ -15,6 +15,10 @@ mkdir faces-np64
 mkdir faces-np64_edge
 mkdir faces-np64_gray
 python get_face.py patio faces
+for line in `cat blacklist.txt`
+do
+  rm faces/$line
+done
 python get_face.py non-patio faces-np
 python resize.py faces
 python resize.py faces-np
